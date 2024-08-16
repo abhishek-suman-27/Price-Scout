9e3a93d77c68c38e21223fdb74fb4f22ebc3085b
abhishek-suman-27 <99277080+abhishek-suman-27@users.noreply.github.com>
2024-08-16 19:44:26 +0530

    Scraped and cleane the data

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -9,6 +9,11 @@ export async function scrapeAndStoreProduct (productUrl: string) {
     try{
         // Scrape the product page
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+        if(!scrapedProduct){
+            return;
+        }
+
     }
     catch(error: any){
         throw new Error(`Failed to create/update product: ${error.message}`);
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url){
@@ -31,7 +32,66 @@ export async function scrapeAmazonProduct(url: string) {
         // Fetch the product page
         const response = await axios.get(url, options);
 
-        console.log(response.data);
+        // console.log(response.data);
+        const $ = cheerio.load(response.data);
+
+        // Extract the product title
+        const title = $('#productTitle').text().trim();
+
+        // Extract the product price
+        const currentPrice = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+            $('.a-price.a-text-price')
+        );
+
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('#priceblock_dealprice'),
+            $('.a-price.a-text-price span.a-offscreen'),
+            $('#listPrice'),
+            $('.a-size-base.a-color-price')
+        );
+
+        const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+
+        const image = 
+            $('#limgBlkFront').attr('data-a-dynamic-image') || 
+            $('#landingImage').attr('data-a-dynamic-image') ||
+            '{}'
+
+        const imageUrls = Object.keys(JSON.parse(image));
+
+        const currency = extractCurrency($('.a-price-symbol'));
+        
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+
+        const description = extractDescription($);
+        // Construct data object with scraped Information
+
+        const data = {
+            url,
+            currency: currency || '₹',
+            image: imageUrls[0],
+            title,
+            currentPrice: Number(currentPrice) || Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: 100,
+            stars: 4.5,
+            isOutOfStock: outOfStock,
+            description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            average: Number(currentPrice) || Number(originalPrice),
+        }
+
+        // return data;
+        console.log(data);
     }
     catch(error: any){
         throw new Error(`Failed to create/update product: ${error.message}`);
